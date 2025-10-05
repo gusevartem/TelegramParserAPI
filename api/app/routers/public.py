2adab4dbcf1bb5e564d8a95db903a1122e09b9b8
ycalk <alosev752@gmail.com>
2025-10-05 12:53:09 +0500

    Add methods into api: /get_media, /get_messages

--- api/app/routers/public.py
+++ api/app/routers/public.py
@@ -3,12 +3,15 @@ from app.config import ApiServiceConfig
 from fastapi import APIRouter, HTTPException
 from shared_models.database.get_channel import GetChannelRequest, GetChannelResponse
 from shared_models.storage.get_logo import GetLogoRequest
+from shared_models.storage.get_media import GetMediaRequest
+from shared_models.database.get_media import GetMediaRequest as GetMediaDatabaseRequest
 from shared_models.database.get_channels_ids import GetChannelsIdsResponse
 from shared_models.database.get_24h_statistics import (
     Get24hStatisticsRequest,
     Get24hStatisticsResponse,
     StatisticsSorting,
 )
+from shared_models.database.get_messages import GetMessagesRequest, GetMessagesResponse
 from fastapi import Depends
 from fastapi.responses import StreamingResponse
 from app.services import Database, Storage
@@ -66,5 +69,35 @@ async def get_24h_statistics(channel_id: int):
             channel_id=channel_id, sorting=StatisticsSorting.NEWEST
         )
         return await database_service.get_24h_statistics(req)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.get(
+    "/get_channel_messages",
+    responses=ApiServiceConfig.DEFAULT_RESPONSE,
+    response_model=GetMessagesResponse,
+)
+async def get_channel_messages(channel_id: int):
+    try:
+        return await database_service.get_channel_messages(
+            GetMessagesRequest(channel_id=channel_id)
+        )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.get(
+    "/get_media", responses=get_logo_responses, response_class=StreamingResponse
+)
+async def get_media(request: GetMediaRequest = Depends()):
+    try:
+        media_data = (await storage_service.get_media(request)).media
+        media_model = await database_service.get_media(
+            GetMediaDatabaseRequest(media_id=request.media_id)
+        )
+        return StreamingResponse(
+            io.BytesIO(media_data), media_type=media_model.root.mime_type
+        )
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
