5fd10caf7090268e8cda09a9ba8e5eab883af222
hazggg <hazggg@gmal.com>
2025-11-03 11:43:44 +0300

    sentry: add trace

--- database/run.py
+++ database/run.py
@@ -16,6 +16,13 @@ sentry_sdk.init(
     dsn=os.getenv("SENTRY_DSN"),
     environment=os.getenv("SENTRY_ENV"),
     send_default_pii=True,
+    traces_sample_rate=1.0,
+    # To collect profiles for all profile sessions,
+    # set `profile_session_sample_rate` to 1.0.
+    profile_session_sample_rate=1.0,
+    # Profiles will be automatically collected while
+    # there is an active span.
+    profile_lifecycle="trace",
 )
 
 REDIS_SETTINGS = RedisSettings(
