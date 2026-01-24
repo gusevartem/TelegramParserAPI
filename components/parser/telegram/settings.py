8cc432a92e814cad7707c9698995ca94f1b3d462
ycalk <alosev752@gmail.com>
2026-01-25 03:07:02 +0500

    Добавить настройки по умолчанию telegram
    - Добавить поля для дефолтных api_id, api_hash и других параметров клиента
    - Добавить свойство default_credentials для удобного доступа к дефолтным учетным данным

--- components/parser/telegram/settings.py
+++ components/parser/telegram/settings.py
@@ -1,5 +1,6 @@
 from typing import ClassVar
 
+from parser.dto import TelegramCredentials
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -16,3 +17,23 @@ class TelegramSettings(BaseSettings):
     session_storage_delayed_exchange_name: str = (
         "telegram.session_storage.delayed_exchange"
     )
+
+    default_api_id: int
+    default_api_hash: str
+    default_device_model: str
+    default_system_version: str
+    default_app_version: str
+    default_lang_code: str
+    default_system_lang_code: str
+
+    @property
+    def default_credentials(self) -> TelegramCredentials:
+        return TelegramCredentials(
+            api_id=self.default_api_id,
+            api_hash=self.default_api_hash,
+            device_model=self.default_device_model,
+            system_version=self.default_system_version,
+            app_version=self.default_app_version,
+            lang_code=self.default_lang_code,
+            system_lang_code=self.default_system_lang_code,
+        )
